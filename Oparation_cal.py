20965ea2259e5e1af8635f6a612b57b1a97e2df2
kiran260790 <kiranssgowda333@gmail.com>
2018-10-10 00:09:01 +0530

    Using modules

---
+++ Oparation_cal.py
@@ -0,0 +1,11 @@
+from Cal import calculator
+
+
+num1 = int(input("Enter the First Number : "))
+num2 = int(input("Enter the Second Number : "))
+
+c = calculator(num1,num2)
+print("The Addition of Two Number are :",c.addition())
+print("The Subtration of Two Number are :",c.subtraction())
+print("The Multiplication of Two Number are :",c.multiplication())
+print("The Division of Two Number are :",c.division())
