1e823c281e005c021a4992aa988268c9a12acf8c
kiran260790 <kiranssgowda333@gmail.com>
2018-10-10 00:07:33 +0530

    Class and Functions

---
+++ Cal.py
@@ -0,0 +1,21 @@
+class calculator:
+
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+
+
+    def addition(self):
+        return self.num1+self.num2
+
+    def subtraction(self):
+        return self.num1-self.num2
+
+    def multiplication(self):
+        return self.num1*self.num2
+
+    def division(self):
+        return self.num1//self.num2
+
+
+
